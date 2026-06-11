4fab15c62e7146f1555dc1c65d1ec46afa2a9584
roman.turlay <front_2@galaxy-it.net>
2026-06-11 15:12:50 +0300

    fixed light

--- src/ui/components/atomic/organisms/Configurator/CanvasControl/CanvasControl.tsx
+++ src/ui/components/atomic/organisms/Configurator/CanvasControl/CanvasControl.tsx
@@ -5,12 +5,12 @@ import { ViewControls } from './ViewControls';
 const CanvasControl = () => {
   return (
     <>
-      <ambientLight intensity={0.55} />
+      <ambientLight intensity={1.4} />
       <hemisphereLight args={['#f5f5f5', '#8a8a8a', 0.42]} />
-      <directionalLight position={[5, 10, 5]} intensity={0.82} />
-      <directionalLight position={[-5, 5, -5]} intensity={0.54} />
-      <directionalLight position={[0, -5, 5]} intensity={0.3} />
-      <directionalLight position={[-3, 3, 5]} intensity={0.36} />
+      <directionalLight position={[5, 10, 5]} intensity={0.1} />
+      <directionalLight position={[-5, 5, -5]} intensity={0.1} />
+      <directionalLight position={[0, -5, 5]} intensity={0.1} />
+      <directionalLight position={[-3, 3, 5]} intensity={0.1} />
       <ViewControls />
     </>
   );
